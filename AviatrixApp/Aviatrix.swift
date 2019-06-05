5a4220b318644efd70b9d1ae2ffb8339673d984a
Helen <helen@MacBook-Pro-2.home>
2019-06-04 20:47:25 -0400

    Complete Iteration 2

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -13,6 +13,12 @@ class Aviatrix {
     var author : String
     var location : String
     var data = AviatrixData()
+    var distanceTraveled = 0
+    var maxFuel = 5000
+    var fuelLevel = 5000.0
+    var milesPerGallon = 0.4
+    var galsNeededtoRefuel = 0.0
+    var fuelCost = 0.0
   
     init(authorName : String, loc : String) {
         author = authorName
@@ -31,13 +37,27 @@ class Aviatrix {
     }
     
     
-    func refuel() {
-        
+    func refuel(location : String) {
+        galsNeededtoRefuel = amtFueled()
+        fuelCost = fuelCost + (amtFueled())*(data.fuelPrices[location]!)
+        fuelLevel = 5000.0
+    }
+    
+    func amtFueled() -> Double{
+        return 5000.0-fuelLevel
     }
     
     func flyTo(destination : String) {
+        distanceTraveled = distanceTraveled+data.knownDistances[location]![destination]!
+        fuelLevel = fuelLevel - (Double(data.knownDistances[location]![destination]!))/milesPerGallon
         location = destination
     }
+    
+//    func fuelCost(location : String) -> Double {
+//
+//        fuelCost = fuelCost + (amtFueled())*(data.fuelPrices[location]!)
+//        return fuelCost
+//    }
     
     func distanceTo(location : String, target : String) -> Int {
         return data.knownDistances[location]![target]!
@@ -45,5 +65,9 @@ class Aviatrix {
     
     func knownDestinations() -> [String] {
        return ["St. Louis", "Phoenix", "Denver", "SLC"]
+    }
+    
+    func fuelPrices(location : String) -> Double {
+        return data.fuelPrices[location]!
     }
 }
--- AviatrixApp/main.swift
+++ AviatrixApp/main.swift
@@ -11,13 +11,13 @@ import Foundation
 func gauges(myPlane : Aviatrix) {
     print("Reading the gauges...")
     print(" ")
-//    print("| Running:  | ✅")
+    print("| Running:   | ✅")
     print("| Location:  | \(myPlane.location)")
-//    print("| Distance:  | \(myPlane.distanceTraveled) miles")
-//    print("| Fuel:      | \(myPlane.fuelLevel) gallons")
-//    print("| Max Fuel:  | \(myPlane.maxFuel) gallons")
-//    print("| MPG:       | \(myPlane.milesPerGallon)")
-//    print("| Fuel Bill: | \(myPlane.fuelCost)")
+    print("| Distance:  | \(myPlane.distanceTraveled) miles")
+    print("| Fuel:      | \(myPlane.fuelLevel) gallons")
+    print("| Max Fuel:  | \(myPlane.maxFuel) gallons")
+    print("| MPG:       | \(myPlane.milesPerGallon)")
+    print("| Fuel Bill: | \(myPlane.fuelCost)")
 }
 
 func fly(myPlane : Aviatrix) {
@@ -52,26 +52,27 @@ func fly(myPlane : Aviatrix) {
 }
 
 func refuel(myPlane : Aviatrix) {
-    let refuelData = myPlane.refuel()
     
+    let price = myPlane.fuelPrices(location : myPlane.location)
     print("Refueling...")
-    print("⛽ Here in _________, jet fuel costs _________")
-    print("⛽ You refueled _________ gallons totaling _________")
+    print("⛽ Here in \(myPlane.location), jet fuel costs $\(price)")
+    print("⛽ You refueled \(myPlane.amtFueled()) gallons totaling $\((myPlane.amtFueled())*(price))")
+    
+    let refuelData = myPlane.refuel(location : myPlane.location)
 }
 
 func fuelCheck(myPlane : Aviatrix, destination : String) -> Bool {
-//    let distanceToTravel =  Double(myPlane.distanceTo(target : destination))
-//    if myPlane.fuelLevel < distanceToTravel {
-//        print(" ")
-//        print("🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥")
-//        print("Oh no! You've run out of fuel and crashed on the way to \(myPlane.location)!")
-//        print("🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥")
-//
-//        return false
-//    } else {
-//        return true
-//    }
-    return true
+    let distanceToTravel =  Double(myPlane.distanceTo(location: plane.location, target : destination))
+    if myPlane.fuelLevel < distanceToTravel {
+        print(" ")
+        print("🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥")
+        print("Oh no! You've run out of fuel and crashed on the way to \(myPlane.location)!")
+        print("🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥")
+
+        return false
+    } else {
+        return true
+    }
 }
 
 
